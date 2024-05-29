6206fb01e22af6b5fbfae4d18c7df32587a783fd
Nehaa7255 <nehamas04@gmail.com>
2024-05-29 17:57:43 +0530

    Project 1 completed.

---
+++ project1/src/App.jsx
@@ -0,0 +1,11 @@
+import Greeting from "./components/Greeting"
+
+const App = () => {
+  return (
+    <div>
+    <Greeting name={" Neha"}/>
+    </div>
+  )
+}
+
+export default App
---
+++ project1/src/components/Greeting.jsx
@@ -0,0 +1,10 @@
+const Greeting = (props) => {
+  return (
+    <>
+    <div>Hello,{props.name}</div>
+
+    </>
+  )
+}
+
+export default Greeting
