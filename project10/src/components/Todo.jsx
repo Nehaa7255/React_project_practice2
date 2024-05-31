eff8b4174224e903e45609631eb73744f51a83e5
Nehaa7255 <nehamas04@gmail.com>
2024-05-31 17:23:25 +0530

    Project 10 created.

--- project10/src/components/Todo.jsx
+++ project10/src/components/Todo.jsx
@@ -5,10 +5,7 @@ const Todo = (todoItems) => {
     <div>
     <h1>to-do</h1>
     {todoItems.map((Todo) =>(
-        <heading>{Todo.items}</heading>
 <p>{Todo.description}</p>
-
-
     ))}
 
     </div>
