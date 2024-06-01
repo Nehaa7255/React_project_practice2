b8b2deed0c329826321e75ab1e63ba91bd338674
Nehaa7255 <nehamas04@gmail.com>
2024-06-01 18:46:46 +0530

    Project 10 completed.  Practice2 project completed.

--- project10/src/components/Todo.jsx
+++ project10/src/components/Todo.jsx
@@ -1,16 +1,16 @@
 
 const Todo = (todoItems) => {
+  console.log(todoItems);
   return (
     <>
     <div>
-    <h1>to-do</h1>
-    {todoItems.map((Todo) =>(
-<p>{Todo.description}</p>
-    ))}
+    <h1>to-do App</h1>
+    {todoItems.todoItems.map((Todo) =>(
 
+<p key={Todo.id} style={{color: Todo.isCompleted ? "green" :"red"}}>{<h4>{Todo.title} </h4>} {Todo.description}</p>
+    ))}
     </div>
     </>
   )
 }
-
 export default Todo
